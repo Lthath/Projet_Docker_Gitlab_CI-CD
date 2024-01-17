2d07709a5cb71bb32b44048b088706fdcbfec961
Lthath <hakim.afarmach@supdevinci-edu.fr>
2024-01-17 14:20:10 +0100

    Ajout du footer + ajustement police

--- my-static-app-car/src/assets/index.js
+++ my-static-app-car/src/assets/index.js
@@ -1,6 +1,15 @@
 import logo from "./logoipsum.png"
 import background from "./background.jpg"
+import fb from "./fb.png"
+import linkedin from "./linkedin.png";
+import insta from "./insta.png";
+import twitter from "./twitter.png";
+
 export {
   logo,
-  background
+  background,
+  fb,
+  insta,
+  linkedin,
+  twitter
 }
